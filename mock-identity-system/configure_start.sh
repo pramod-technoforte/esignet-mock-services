375df541fda6243111601dcd3aa279d460dc0c1d
shubham-technoforte <125962820+shubham-technoforte@users.noreply.github.com>
2023-09-08 17:00:18 +0530

    Update configure_start.sh

--- mock-identity-system/configure_start.sh
+++ mock-identity-system/configure_start.sh
@@ -37,5 +37,8 @@ cd ./$DIR_NAME && chmod +x install.sh && sudo ./install.sh
 echo "Installation complete"
 cd $work_dir
 echo "************"
-exec "$@"
+#exec "$@"
+for arg in "$@"; do
+    echo "$arg"
+done
 echo "#############"
